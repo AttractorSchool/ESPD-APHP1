43eb6d9e9ed78b2ac8ffc968daed05150c62eac9
Timur <offtim21@gmail.com>
2023-07-06 15:13:40 +0600

    Resolves #3: Внёс поправки в верстку

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,7 @@
     <link href="{{ asset('css/styles.css') }}" rel="stylesheet">
     <link href="{{ asset('css/media.css') }}" rel="stylesheet">
         <div class="row justify-content-center">
-            <div class="promo" style="background-image: url('{{ asset('storage/pictures/Rectangle.png') }}');">
+            <div class="promo">
                 <h1>Сообщество близких по духу предпринимателей.</h1>
                 <p class="promo-text"> Нам по пути, если бизнес для тебя не просто деньги,а помощь другим и личная миссия</p>
                 <div class="block-card">
@@ -40,7 +40,7 @@
                         <div class="img">
                             <img src="https://vokrug.tv/pic/news/f/b/9/2/fb927aca1ca4a42d9bd46ce4e3330bdd.jpg" alt="img" draggable="false">
                         </div>
-                        <h2>{{ $review->user->name }}</h2>
+                        <h2 style="overflow: hidden">{{ $review->user->name }}</h2>
                         <span>{{ $review->body }}</span>
                     </li>
                 @endforeach
@@ -48,7 +48,7 @@
             <i id="right" class="fa-solid fa-angle-right"></i>
         </div>
         <div class="residents">
-            <h2>Наши резиденты</h2>
+            <h2 style="overflow:hidden;">Наши резиденты</h2>
             <div class="carousel-wrapper">
                 <i id="left" class="fa-solid fa-angle-left icon-left"></i>
 
@@ -61,7 +61,7 @@
                                     <p>{{ $review->body }}</p>
                                 @endforeach
                             </div>
-                            <h3>{{ $user->name }}</h3>
+                            <h3 style="overflow:hidden;">{{ $user->name }}</h3>
                         </div>
                     @endforeach
 
@@ -76,7 +76,7 @@
         </div>
         <div class="event-calendar">
             <div class="event-info">
-                <h2>Получи полный календарь мероприятий</h2>
+                <h2 style="overflow: hidden">Получи полный календарь мероприятий</h2>
                 <p>Получи подробную презентацию, календарь мероприятий клуба, информацию о резидентах и возможностях нашей
                     платформы
                 </p>
@@ -84,17 +84,64 @@
             </div>
 
         </div>
-        <h1 class="text-center mt-5">Выбрать свой формат участия</h1>
+        <h1 class="text-center mt-5 price-text">Выбрать свой формат участия</h1>
         <div class="d-flex row p-3">
             @foreach($subscriptions as $subscription)
                 @if($subscription->type=='free')
                     <div class="card my-2 text-center">
                         <div class="card-body">
                             <h5 class="card-title">Получить консультацию</h5>
-                            <p class="card-text">Оставить заявку, чтобы мы ответили на все вопросы и рассказали о жизни
+                            <p class="card-text" style="color: black">Оставить заявку, чтобы мы ответили на все вопросы и рассказали о жизни
                                 клуба</p>
                             <h5>Бесплатно</h5>
-                            <a href="#" class="btn btn-warning rounded-5">Оставить заявку</a>
+
+                            <!-- Button trigger modal -->
+                            <button type="button" class="btn btn-warning rounded-5" data-bs-toggle="modal"
+                                    data-bs-target="#exampleModal">
+                                Оставить заявку
+                            </button>
+
+                            <!-- Modal -->
+                            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel"
+                                 aria-hidden="true">
+                                <div class="modal-dialog">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h1 class="modal-title fs-5" id="exampleModalLabel" style="color: black">Получить
+                                                консультацию</h1>
+                                            <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                                    aria-label="Close"></button>
+                                        </div>
+                                        <div class="modal-body">
+                                            <form action="{{route('front.form')}}" method="POST">
+                                                @csrf
+                                                <div class="u-form-group u-form-name my-4">
+                                                    <input type="text" placeholder="Введите свое имя" name="name"
+                                                           class="col-12 form-control"
+                                                           required>
+                                                </div>
+                                                <div class="u-form-email u-form-group my-4">
+                                                    <input type="email" placeholder="Введите свой email"
+                                                           name="email"
+                                                           class="col-12 form-control"
+                                                           required>
+                                                </div>
+                                                <div class="u-form-group u-form-message my-4">
+                                            <textarea placeholder="Введите свое сообщение" rows="4" cols="50"
+                                                      name="message"
+                                                      class="col-12 form-control"
+                                                      required></textarea>
+                                                </div>
+                                                <button type="submit"
+                                                        class="my-4 btn btn-secondary">
+                                                    Оставить данные
+                                                </button>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
                         </div>
                     </div>
                 @else
@@ -103,7 +150,12 @@
                             <h5 class="card-title">{{$subscription->type}}</h5>
                             <p class="card-text">{{$subscription->description}}</p>
                             <h5>{{$subscription->price}} $</h5>
-                            <a href="#" class="btn btn-warning rounded-5">Выбрать тариф</a>
+
+                            <button type="button" class="btn btn-warning rounded-5" data-bs-toggle="modal"
+                                    data-bs-target="#exampleModal">
+                                Оставить заявку
+                            </button>
+                            {{--                            <a href="#" class="btn btn-warning rounded-5">Выбрать тариф</a>--}}
                         </div>
                     </div>
                 @endif
