1f3d869f81294a755e06935516cf7435c01f9057
Amalbek <aralbayamalbek@gmail.com>
2023-07-13 11:50:46 +0600

    Resolves #16: Реализовать страницу с чатом

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ActionController;
+use App\Http\Controllers\ChatController;
 use App\Http\Controllers\Front\FormController;
 use App\Http\Controllers\Front\PageController;
 use Illuminate\Support\Facades\Auth;
@@ -21,5 +22,13 @@ Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('ho
 Route::get('/residents', [PageController::class, 'residents'])->name('residents');
 Route::post('/form', [FormController::class, 'store'])->name('front.form');
 Route::post('/connect', [ActionController::class, 'connect'])->name('connect');
+
+Route::post('/message', [ChatController::class, 'send'])->name('chat.send');
+Route::get('/message/{id}', [ChatController::class, 'show'])->name('chat.show');
+Route::get('/messages/{id}', [ChatController::class, 'showBlade'])->name('show');
+
+Route::get('/refresh/{model}', [ActionController::class, 'update'])->name('refresh');
+Route::post('/confirm', [ActionController::class, 'confirm'])->name('confirm');
+
 Auth::routes();
 
