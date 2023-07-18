44d9594a4c8cef23aad8afb820151f232d45a765
Ildar <khalfin94@gmail.com>
2023-07-18 23:59:14 +0600

    Resolved #36 Создать таблицы (Роли, интересы, тестирование, подписки)

---
+++ app/Models/Rating.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Rating extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['user_id', 'rated_by_user_id', 'rating'];
+
+    /**
+     * @return BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * @return BelongsTo
+     */
+    public function ratedByUser(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'rated_by_user_id');
+    }
+}
