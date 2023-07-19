a01292ae561e1347c036274a04614c69be332db0
Арыстанбек <hydro_oxide@mail.ru>
2023-07-20 03:04:30 +0600

    #30 make ChatBlade

--- app/Models/Response.php
+++ app/Models/Response.php
@@ -37,23 +37,4 @@ class Response extends Model
     {
         return $this->messages()->latest()->first();
     }
-
-    public function print($link): void
-    {
-        $html = '
-       <a href="' . $link . '" class="list-group-item list-group-item-action d-flex justify-content-between align-items-center">
-            <div class="d-flex row col-11">
-                <img src="https://media.sproutsocial.com/uploads/2022/06/profile-picture.jpeg" alt="Иван" class="rounded-5 col-3 m-0" style="height: 3.8rem">
-                <div class="d-flex flex-column col-9 m-0">
-                    <span class="ml-2 fs-6">Иван</span>
-                    <p class="text-dark text-start fst-italic" style="height: 2rem">' . $this->last_Message()->body . '</p>
-                </div>
-            </div>
-            <span class="badge badge-primary badge-pill text-dark">' . $this->last_Message()->created_at->format(
-                'D'
-            ) . '</span>
-        </a>';
-
-        print $html;
-    }
 }
--- resources/views/front/friends.blade.php
+++ resources/views/front/friends.blade.php
@@ -2,19 +2,21 @@
 @section('content')
 
     <div>
-        {{--        style="margin-top: rem "--}}
-        {{--        <div class="d-flex">--}}
-        {{--            <h2 class="text-dark">Chat WCG</h2>--}}
-        {{--        </div>--}}
         @if($responses->isNotEmpty())
             <div class="list-group mt-3">
                 @foreach($responses as $response)
-                    {{--                    {{$response->print(route('showChat',['id'=>$response->id]))}}--}}
                     <a href="{{route('showChat',['id'=>$response->id])}}"
                        class="list-group-item list-group-item-action d-flex justify-content-between align-items-center">
                         <div class="d-flex row col-11">
-                            <img src="https://media.sproutsocial.com/uploads/2022/06/profile-picture.jpeg" alt="Иван"
-                                 class="rounded-5 col-3 m-0" style="height: 3.8rem">
+                            @if($response->first->id==Auth::user()->id)
+                                <img src="{{ asset('storage/' . $response->second->avatar) }}" alt="Avatar"
+                                     class="rounded-5 col-3 m-0"
+                                     style="height: 3.8rem">
+                            @else
+                                <img src="{{ asset('storage/' . $response->first->avatar) }}" alt="Avatar"
+                                     class="rounded-5 col-3 m-0"
+                                     style="height: 3.8rem">
+                            @endif
                             <div class="d-flex flex-column col-9 m-0">
                                 @if($response->first->id==Auth::user()->id)
                                     <span class="ml-2 fs-6">{{$response->second->name}}</span>
@@ -22,11 +24,21 @@
                                     <span class="ml-2 fs-6">{{$response->first->name}}</span>
                                 @endif
                                 <p class="text-dark text-start fst-italic"
-                                   style="height: 2rem">{{$response->last_Message()->body}}</p>
+                                   style="height: 2rem">
+                                    @if(isset($response->last_Message()->body))
+                                        {{$response->last_Message()->body}}
+                                    @else
+                                        Нету сообщении
+                                    @endif
+                                </p>
                             </div>
                         </div>
                         <span
-                            class="badge badge-primary badge-pill text-dark">{{$response->last_Message()->created_at->format('D')}}
+                            class="badge badge-primary badge-pill text-dark">
+                            @if(isset($response->last_Message()->body))
+                                {{$response->last_Message()->created_at->format('D')}}
+                            @else
+                            @endif
                         </span>
                     </a>
                 @endforeach
