ebc07b62b7e740ae56b0a53e396e7adf3c5b6871
Timur <offtim21@gmail.com>
2023-08-07 12:49:08 +0600

    #81 - Realization of first page of events

--- database/factories/EventFactory.php
+++ database/factories/EventFactory.php
@@ -3,6 +3,8 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Storage;
+use Intervention\Image\Facades\Image;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Event>
@@ -18,11 +20,22 @@ class EventFactory extends Factory
     {
         return [
             'title' => $this->faker->sentence,
-            'description' => $this->faker->paragraph,
+            'description' => $this->faker->sentence,
             'date' => $this->faker->date,
             'location' => $this->faker->address,
             'format' => $this->faker->randomElement(['online', 'offline']),
             'price' => $this->faker->randomFloat(2, 0, 1000),
+            'picture' => $this->getImage(rand(1,5))
         ];
+    }
+
+    private function getImage(int $imageNumber = 1): string
+    {
+        $path = storage_path() . "/avatars/" . $imageNumber . ".jpeg";
+        $imageName = md5($path) . '.jpeg';
+        $image = 'pictures/' . $imageName;
+        $resize = Image::make($path)->fit(300)->encode('jpeg');
+        Storage::disk('public')->put('pictures/'.$imageName, $resize->__toString());
+        return $image;
     }
 }
