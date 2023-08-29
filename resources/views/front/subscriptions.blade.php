8ace5738f15bd552be8e04ebac186f91a1d5d2c9
Ildar <khalfin94@gmail.com>
2023-08-29 19:43:48 +0600

    #99 Added services, fixed subscription controller and views

--- resources/views/front/subscriptions.blade.php
+++ resources/views/front/subscriptions.blade.php
@@ -10,7 +10,6 @@
                     <h2>{{ $subscription->type }}</h2>
                     <p class="text-dark">{{ $subscription->description }}</p>
                     <p class="text-dark">Цена: {{ round($subscription->price) }}</p>
-                    @auth
                         <a class="subscribe-btn btn btn-primary"
                            href="{{ route('payment', ['type' => $subscription->type,
                         'subscriptionId' => $subscription->id,
@@ -18,10 +17,6 @@
                         >
                             Подписаться
                         </a>
-                    @else
-                        <p>Чтобы подписаться, необходимо войти в свой аккаунт.</p>
-                    @endauth
-
                 </div>
             @endforeach
         </div>
